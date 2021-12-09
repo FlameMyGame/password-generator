b3f00c553277d48e1d2dcae5d9ba35e3cd3a9991
Dawson Gunnell <dlgunnell@outlook.com>
2021-12-09 10:02:25 -0700

    adding variables and functions

--- script.js
+++ script.js
@@ -2,11 +2,23 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var passwordLength = function getRandomLenth(min, max) {
+    min = Math.ceil(8);
+    max = Math.floor(128);
+    return Math.floor(Math.random() * (max - min) + min);
 
-  passwordText.value = password;
+}
+
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var numbers = "1234567890";
+    var special = "!@#$%^&*()";
+    var passwordLength = randomLength
+
+    passwordText.value = password;
 
 }
 
